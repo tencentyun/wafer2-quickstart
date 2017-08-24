2e07e9849202e37449b45f79dea3684aa5a3b777
jasonelchen <jasonelchen@tencent.com>
2017-08-24 16:51:20 +0800

    新增快速添加 CGI 指引

---
+++ client/pages/addCgi/addCgi.js
@@ -0,0 +1,29 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        requestResult: ''
+    },
+
+    testCgi: function () {
+        util.showBusy('请求中...')
+        var that = this
+        qcloud.request({
+            url: `https://${config.service.host}/weapp/demo`,
+            login: true,
+            success (result) {
+                util.showSuccess('请求成功完成')
+                that.setData({
+                    requestResult: JSON.stringify(result.data)
+                })
+            },
+            fail (error) {
+                util.showModel('请求失败', error);
+                console.log('request fail', error);
+            }
+        })
+    }
+})
