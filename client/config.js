be17941cd7f120948ebee0c09fb4ca0b1accb82b
Jason Chen <ceocjy@vip.qq.com>
2017-10-16 08:22:38 -0500

    修复404

--- client/config.js
+++ client/config.js
@@ -12,16 +12,16 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `${host}/login`,
+        loginUrl: `${host}/weapp/login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/user`,
+        requestUrl: `${host}/weapp/user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `${host}/tunnel`,
+        tunnelUrl: `${host}/weapp/tunnel`,
 
         // 上传图片接口
-        uploadUrl: `${host}/upload`
+        uploadUrl: `${host}/weapp/upload`
     }
 };
 
